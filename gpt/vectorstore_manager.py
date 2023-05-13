a21a9ca073259cbed1282c82c4cf3ccf5cd8e291
Megham Garg <gargmegham3@gmail.com>
2023-05-13 17:00:32 +0530

    create_documents return missing

--- gpt/vectorstore_manager.py
+++ gpt/vectorstore_manager.py
@@ -20,6 +20,7 @@ class VectorStoreManager:
             model_name=tokenizer_model,
         ).split_text(text)
         await cache.vectorstore.aadd_texts(texts=texts)
+        return texts
 
     @staticmethod
     async def asimilarity_search(
