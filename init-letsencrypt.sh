473fbdcb90ddfaefc2729098f08e5fa993e91690
Megham Garg <gargmegham3@gmail.com>
2023-05-26 13:37:35 +0530

    update init script ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(chat.pubtrawlr.com)
 rsa_key_size=4096
 data_path="./certbot"
-email="" # Adding a valid address is strongly recommended
+email="meghamgarg@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
