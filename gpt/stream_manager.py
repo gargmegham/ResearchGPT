779a5d79045fa7b17184c5ab0ce58a175131a198
Megham Garg <gargmegham3@gmail.com>
2023-05-30 16:42:03 +0530

    disable worker timeout

--- gpt/stream_manager.py
+++ gpt/stream_manager.py
@@ -212,7 +212,7 @@ class ChatGptStreamManager:
         else:
             # if received chatroom_id is in chatroom_ids, get context from memory
             buffer.change_context_to(index=index)
-            loop = asyncio.get_event_loop()
-            loop.create_task(
-                SendToWebsocket.init(buffer=buffer, send_chatroom_ids=False)
+            await SendToWebsocket.init(
+                buffer=buffer,
+                send_chatroom_ids=False,
             )
