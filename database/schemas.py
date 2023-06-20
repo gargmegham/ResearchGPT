489c6222c90d8d9920f4bdf8624334f115c1ad2c
Megham Garg <95271253+gargmegham@users.noreply.github.com>
2023-06-20 12:51:14 +0530

    Update schemas.py

--- database/schemas.py
+++ database/schemas.py
@@ -45,51 +45,6 @@ class InitMessage(BaseModel):
     init_callback: bool = True
 
 
-class PubMedPaper(BaseModel):
-    """
-    PubMed paper:
-      - pmid: "36960329",
-      - volume: "7",
-      - issue: "",
-      - year: "2023",
-      - month: "",
-      - day: "",
-      - pages: "100179",
-      - issn: "2666-6235",
-      - journal: "Journal of migration and health",
-      - journalabbrev: "J Migr Health",
-      - title: "Foreigners living in Tuscany at the time of coronavirus outbreak.",
-      - abstract: "During the coronavirus outbreak, a worldwide state of emergency and lockdown significantly affected the volunteer services for foreigners. The SARS-CoV-2 surveillance program was strengthened among migrants arriving in Italy. However, few screening measures for SARS-CoV2 infection have been conducted on the foreign population already present in Italy. In Tuscany, a great effort was made to know the epidemiological features of coronavirus outbreaks in the foreigners. Based on these premises, this study describes the prevalence and characteristics of SARS-CoV-2 infection in foreigners present in the Tuscan territory during the months of the highest incidence of this pandemic.",
-      - affiliation: "",
-      - authors: "Silvestri C, Profili F, Bartolacci S, Voller F, Stasi C",
-      - articleid: "36960329,PMC10022458,10.1016/j.jmh.2023.100179,S2666-6235(23)00029-6",
-      - keywords: "",
-      - lastname: "Silvestri, Profili, Bartolacci, Voller, Stasi",
-      - put_in_tables: "<a href=\"https://pubmed.ncbi.nlm.nih.gov/36960329\">Foreigners living in Tuscany at the time of coronavirus outbreak.</a>",
-      - date: "2023-01-01"
-    """
-
-    pmid: str | None
-    year: str | None
-    day: str | None
-    month: str | None
-    volume: str | None
-    issue: str | None
-    title: str
-    affiliation: str | None
-    abstract: str
-    pages: str | None
-    issn: str | None
-    journalabbrev: str | None
-    authors: str
-    journal: str | None
-    date: str | None
-    put_in_tables: str | None
-    keywords: str | None
-    articleid: str | None
-    lastname: str | None
-
-
 class MessageToWebsocket(BaseModel):
     """
     message to send to websocket
