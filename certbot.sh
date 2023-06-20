b126a9fb820b5b4aac403a7c28de538781be3820
Megham Garg <95271253+gargmegham@users.noreply.github.com>
2023-06-20 10:45:33 +0530

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(chat.pubtrawlr.com)
+domains=(chat.meghamgarg.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="meghamgarg@gmail.com" # Adding a valid address is strongly recommended
